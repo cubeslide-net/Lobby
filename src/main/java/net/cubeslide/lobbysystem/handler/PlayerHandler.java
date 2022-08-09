f744c1f357c489437242609983b6d28a80567a2a
Timeox2k <schockilp@gmail.com>
2022-08-10 00:03:27 +0200

    Fixed someone being lost, giving possibility to grief .-.

--- src/main/java/net/cubeslide/lobbysystem/handler/PlayerHandler.java
+++ src/main/java/net/cubeslide/lobbysystem/handler/PlayerHandler.java
@@ -10,6 +10,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.*;
 import org.bukkit.event.weather.WeatherChangeEvent;
@@ -78,16 +79,21 @@ public class PlayerHandler implements Listener {
     @EventHandler
     public void on(PlayerInteractEvent event) {
         final Player player = event.getPlayer();
-        final UUID playerUUID = player.getUniqueId();
         if (player.getItemInHand().getType() == Material.ENDER_PEARL) {
             event.setCancelled(false);
             playerUsedEP.put(player.getUniqueId(), 30);
-            return;
         }
+    }
+
+    @EventHandler
+    public void onBreak(BlockBreakEvent event) {
+        final Player player = event.getPlayer();
+        final UUID playerUUID = player.getUniqueId();
         if (!BuildCMD.getInBuildMode().contains(playerUUID)) {
             event.setCancelled(true);
         }
     }
+
 
     @EventHandler
     public void on(PlayerDropItemEvent event) {
